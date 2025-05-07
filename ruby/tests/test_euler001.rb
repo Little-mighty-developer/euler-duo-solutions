6a024f5eabc85bcbe1b50ac6fa4b63c8693070da
Little-mighty-developer <mandy.austin.arthur@gmail.com>
2025-05-07 17:23:15 +0200

    fix: ruby linter

--- ruby/tests/test_euler001.rb
+++ ruby/tests/test_euler001.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'simplecov'
 SimpleCov.start do
   enable_coverage :branch
@@ -14,11 +16,11 @@ class TestEuler001 < Minitest::Test
     assert_equal 0, sum_multiples_of_3_and_5(3)
     assert_equal 3, sum_multiples_of_3_and_5(4)
     assert_equal 8, sum_multiples_of_3_and_5(6)
-    assert_equal 233168, sum_multiples_of_3_and_5(1000)
+    assert_equal 233_168, sum_multiples_of_3_and_5(1000)
   end
 
   def test_edge_cases
     assert_equal 0, sum_multiples_of_3_and_5(0)
     assert_raises(ArgumentError) { sum_multiples_of_3_and_5(-1) }
   end
-end 
+end
