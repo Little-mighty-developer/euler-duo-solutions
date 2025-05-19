399b029ff6c57adda7367e6605615216ae5748ad
Little-mighty-developer <mandy.austin.arthur@gmail.com>
2025-05-19 16:23:56 +0200

    Palindrome

--- ruby/tests/test_euler004.rb
+++ ruby/tests/test_euler004.rb
@@ -1,27 +1,32 @@
-#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-require "test/unit"
-require_relative "../euler004"
+# !/usr/bin/env ruby
 
+require 'test/unit'
+require_relative '../euler004'
+
+# Test cases for Project Euler Problem 4: Largest Palindrome Product
 class TestEuler004 < Test::Unit::TestCase
-  def test_palindrome
-    # Test single digit numbers
+  def test_single_digit_palindromes
     assert(palindrome?(1))
     assert(palindrome?(9))
+  end
 
-    # Test two digit numbers
+  def test_two_digit_palindromes
     assert(palindrome?(11))
     assert(palindrome?(99))
     assert_false(palindrome?(12))
     assert_false(palindrome?(98))
+  end
 
-    # Test three digit numbers
+  def test_three_digit_palindromes
     assert(palindrome?(121))
     assert(palindrome?(999))
     assert_false(palindrome?(123))
     assert_false(palindrome?(998))
+  end
 
-    # Test larger numbers
+  def test_larger_palindromes
     assert(palindrome?(9009))
     assert(palindrome?(12_321))
     assert_false(palindrome?(12_345))
@@ -29,7 +34,7 @@ class TestEuler004 < Test::Unit::TestCase
 
   def test_largest_palindrome_product
     result = largest_palindrome_product
-    assert_equal(906_609, result)  # The actual answer
+    assert_equal(906_609, result) # The actual answer
 
     # Verify that the result is a palindrome
     assert(palindrome?(result))
@@ -37,14 +42,14 @@ class TestEuler004 < Test::Unit::TestCase
     # Verify that the result is a product of two 3-digit numbers
     factors_found = false
     999.downto(100) do |i|
-      if (result % i).zero?
-        other_factor = result / i
-        if other_factor >= 100 && other_factor <= 999
-          factors_found = true
-          break
-        end
+      next unless (result % i).zero?
+
+      other_factor = result / i
+      if other_factor >= 100 && other_factor <= 999
+        factors_found = true
+        break
       end
     end
     assert(factors_found)
   end
-end 
+end
