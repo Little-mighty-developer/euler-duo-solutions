9b01f1a2e9426506c8c75633c7671f233816946f
Little-mighty-developer <mandy.austin.arthur@gmail.com>
2025-05-19 22:38:49 +0200

    rebase

--- ruby/solutions/euler004.rb
+++ ruby/solutions/euler004.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # !/usr/bin/env ruby
 
 # Project Euler Problem 4: Largest Palindrome Product
@@ -7,8 +9,8 @@
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-def palindrome?(n)
-  n.to_s == n.to_s.reverse
+def palindrome?(number)
+  number.to_s == number.to_s.reverse
 end
 
 def largest_palindrome_product
@@ -17,12 +19,11 @@ def largest_palindrome_product
     i.downto(100) do |j|
       product = i * j
       break if product < largest_palindrome
+
       largest_palindrome = product if palindrome?(product)
     end
   end
   largest_palindrome
 end
 
-if __FILE__ == $PROGRAM_NAME
-  puts largest_palindrome_product
-end 
+puts largest_palindrome_product if __FILE__ == $PROGRAM_NAME
